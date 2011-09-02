154e8b2a8a01bd2f5c4f6dc605ad231d67f30681
Judy Tuan <judytuna@gmail.com>
2011-09-02 07:58:37 -0700

    oops, restarting

--- sc_vods/app/models/vod.rb
+++
@@ -1,8 +0,0 @@
-#where are my fields? it just knows about the stuff in the database lol
-
-#activerecord closely tied to database... it'll use whatever in its database
-
-#stores the data (keeps track of the urls, etc). real functionality is in the controller
-# controller talks to th emodel
-class Vod < ActiveRecord::Base
-end
--- sc_vods/test/unit/helpers/vods_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class VodsHelperTest < ActionView::TestCase
-end
