f9d8d09eb471304005cc71e679a1bd1c498abad6
Judy Tuan <judytuna@gmail.com>
2011-09-03 01:56:04 -0700

    has player and game classes, not linked

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -14,6 +14,7 @@ class PlayersController < ApplicationController
   # GET /players/1.xml
   def show
     @player = Player.find(params[:id])
+    @title = CGI.escapeHTML(@player.name)
 
     respond_to do |format|
       format.html # show.html.erb
